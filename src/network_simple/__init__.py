792d78bb035e7c1289e2d7898cb32b770ba992c1
Matthew Davidson <122948876+generalmattza@users.noreply.github.com>
2024-02-28 14:34:56 -0800

    Update __init__.py

--- src/network_simple/__init__.py
+++ src/network_simple/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 from network_simple.client import SimpleClient, SimpleClientTCP, SimpleClientUDP
 from network_simple.server import SimpleServer, SimpleServerTCP, SimpleServerUDP
